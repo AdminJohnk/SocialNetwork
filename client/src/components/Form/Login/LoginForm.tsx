630647057eb2b3c5d7bb02339aac455de041242d
Nguyễn Hoàng Hải <ln26805@gmail.com>
2023-03-13 21:07:53 +0700

    update code

--- client/src/components/Form/Login/LoginForm.tsx
+++ client/src/components/Form/Login/LoginForm.tsx
@@ -9,13 +9,25 @@ import { REGIS_USER_SAGA } from "../../../redux/actionSaga/UserAction";
 const LoginForm = () => {
   const dispatch = useDispatch();
 
+  const [username, setUsername] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  const handleUsername = (e: any) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePassword = (e: any) => {
+    setPassword(e.target.value);
+  };
+
   const handleSubmit = (e: any) => {
     e.preventDefault();
     dispatch(
       REGIS_USER_SAGA({
         userRegister: {
-          userName: "ok",
-          passWord: "Admintck",
+          userName: username,
+          passWord: password,
         },
       })
     );
@@ -39,12 +51,18 @@ const LoginForm = () => {
               name="userName"
               type="text"
               placeholder="Username"
+              onChange={(e) => {
+                handleUsername(e);
+              }}
             />
             <input
               className="form-control w-full h-9 mb-5 p-5"
               name="passWord"
               type="password"
               placeholder="Password"
+              onChange={(e) => {
+                handlePassword(e);
+              }}
             />
             <button
               type="submit"
