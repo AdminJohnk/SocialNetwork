d6a1afc8dc1e8553fe6c054d505f2a3415346892
Mr-LeeB <quangbaokh9a2@gmail.com>
2023-03-31 13:02:42 +0700

    31/03/2023

--- client/src/components/QuillEditor/QuillEditor.tsx
+++ client/src/components/QuillEditor/QuillEditor.tsx
@@ -6,21 +6,41 @@ import { Input } from 'antd';
 
 const QuillEditor = () => {
     const [quill, setQuill] = useState(null);
+    // const [quill, setQuill] = useState<Quill.Quill | null>(null);
     let newQuill: any = null;
-    var toolbarOptions = [['bold', 'italic'], ['link', 'image'],
-        [{ 'list': 'ordered' }, { 'list': 'bullet' }],];
+    const [numImages, setNumImages] = useState<number>(0);
+    const MAX_IMAGES = 1;
+
+    // const handleImageInserted = () => {
+    //     const numImagesInserted = quill?.container.querySelectorAll('img').length || 0;
+    //     setNumImages(numImagesInserted);
+    //     if (numImagesInserted > MAX_IMAGES) {
+    //         quill?.deleteText(-1, 1);
+    //         alert(`You can only insert up to ${MAX_IMAGES} images`);
+    //     }
+    // };
+
+    var toolbarOptions = [
+        ['bold', 'italic'],
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+        ['align',
+            { 'align': ['center'] },
+            { 'align': ['right'] },
+            { 'align': ['justify'] }],
+        ['link', 'image'],
+        ['clean']];
     useEffect(() => {
         if (quill === null) {
             newQuill = new Quill('#editor', {
                 modules: {
                     toolbar: toolbarOptions
                 },
+                placeholder: 'Share your thoughts...',
                 theme: 'snow'
             });
             setQuill(newQuill);
         }
     }, [quill]);
-
     return (
         <div>
             <div id="editor" />
