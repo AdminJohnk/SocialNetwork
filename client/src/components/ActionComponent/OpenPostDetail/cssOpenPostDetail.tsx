5f8f64895de0075669e2e08223c70f97d1ae6754
AdminJohnk <truongtantai07042002@gmail.com>
2023-05-29 23:07:38 +0700

    hehe

--- client/src/components/ActionComponent/OpenPostDetail/cssOpenPostDetail.tsx
+++ client/src/components/ActionComponent/OpenPostDetail/cssOpenPostDetail.tsx
@@ -3,8 +3,6 @@ import { commonColor } from '../../../util/cssVariable/cssVariable';
 
 const StyleTotal = styled.div`
   background-color: ${(props) => props.theme.colorBg1};
-  min-height: calc(100vh - 5rem);
-  max-height: calc(100vh - 5rem);
   color: ${(props) => props.theme.colorText1};
 
   .input {
